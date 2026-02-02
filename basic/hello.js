669560c7fcebf66df9a52936f4f9c04cfc58a527
Arjun-sing8279 <arjunsingh963440@gmail.com>
2026-02-02 14:12:12 +0530

    node.js

---
+++ basic/hello.js
@@ -0,0 +1,5 @@
+//console.log("hello arjun ");
+//console.log(add(5, 10)); Using the add function from Math.js 
+// const math=require("./Math");
+// console.log("value",math(5,10));
+console.log("hello arjun ");
